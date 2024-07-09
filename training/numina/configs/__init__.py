cd8ceb4f029591ad9ce87a4b78cb6bb6e092b2da
Edward Beeching <edbeeching@users.noreply.github.com>
2024-07-09 16:52:59 +0200

    Revert "Revert "Adds finetuning scripts and instructions""

---
+++ training/numina/configs/__init__.py
@@ -0,0 +1,3 @@
+from .data_config import DataConfig
+from .model_config import ModelConfig
+from .sft_config import SFTConfig
---
+++ training/numina/utils/__init__.py
@@ -0,0 +1,11 @@
+from .data import get_datasets
+from .utils import (
+    H4ArgumentParser,
+    apply_chat_template,
+    check_hub_revision_exists,
+    get_checkpoint,
+    get_tokenizer,
+    hf_login,
+    init_wandb_training,
+    push_to_hub_revision,
+)
